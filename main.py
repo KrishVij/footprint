aa1ca11ee69f5589088f6f2a77b71d28692fb330
KrishVij <official.krishvij@gmail.com>
2025-11-09 10:30:18 +0530

    Refactor DNS record check to improve accuracy and readability

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-import subprocess
+# import subprocess
 
-subprocess.run('ipconfig /displaydns > "C:\\Users\\Krish Vij\\dns.txt"', text=True, shell = True)
+# subprocess.run('ipconfig /displaydns > "C:\\Users\\Krish Vij\\dns.txt"', text=True, shell = True)
 
 def check_dns_records() -> None:
+    
     with open ("C:\\Users\\Krish Vij\\dns.txt", "r") as file:
-        contents = file.read()
-        for line in contents.splitlines():
-            if "Record Name . . . . . : google.com" in line :
-                print("Found google.com DNS record")
-    print(contents)
+        content = file.readlines()
+        for line in content:
+            if "www.google.com" in line:
+                print("Found www.google.com DNS record")
 
 def main() -> None:
     check_dns_records()

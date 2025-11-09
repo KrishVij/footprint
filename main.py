c822ff1acb2a80803edab816cc88f759e1ff43fc
KrishVij <official.krishvij@gmail.com>
2025-11-09 11:15:49 +0530

    Refactor check_dns_records function to accept domain parameter and improve DNS record checking logic

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
-# import subprocess
+import subprocess
 
-# subprocess.run('ipconfig /displaydns > "C:\\Users\\Krish Vij\\dns.txt"', text=True, shell = True)
-
-def check_dns_records() -> None:
-    
+def check_dns_records(domain:str) -> None:
+    subprocess.run('ipconfig /displaydns > "C:\\Users\\Krish Vij\\dns.txt"', text=True, shell = True)
+    found = False
     with open ("C:\\Users\\Krish Vij\\dns.txt", "r") as file:
         content = file.readlines()
+        print("Checking DNS records in local cache...")
         for line in content:
-            if "www.google.com" in line:
-                print("Found www.google.com DNS record")
+            if domain in line:
+                print(f"Found {domain} DNS record")
+                if "A" in content[content.index(line) + 15]:
+                    print(f"Record type A found for {domain}")
+                    print(content[content.index(line) + 15])
+                    found = True
+                    break
+                else:
+                    print(f"No A record found for {domain}")
+                    break
 
 def main() -> None:
-    check_dns_records()
+    domain = input("Enter the domain to check DNS records for: ")
+    check_dns_records(domain)
 
 if __name__ == "__main__":
     main()
